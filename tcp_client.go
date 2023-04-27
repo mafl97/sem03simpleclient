9a5e9a0e0eeb099f8282d65c9bbee2518619094c
mafl97 <marief17@uia.no>
2023-04-27 11:24:14 +0000

    huihuohi

--- tcp_client.go
+++ tcp_client.go
@@ -4,17 +4,20 @@ import (
 	"net"
 	"log"
 	"os"
+	"github.com/mafl97/is105sem03/mycrypt"
 )
 
 func main() {
-	conn, err := net.Dial("tcp", "172.17.0.3:9090")
+	conn, err := net.Dial("tcp", "172.17.0.2:16")
 	if err != nil {
 		log.Fatal(err)
 	}
-    
-	log.Println("os.Args[1] = ", os.Args[1])
 
- 	_, err = conn.Write([]byte(os.Args[1]))
+
+	log.Println("os.Args[1] = ", os.Args[1])
+        kryptertMelding := mycrypt.Krypter([]rune(os.Args[1]), mycrypt.ALF_SEM03, 4)
+        log.Println("Kryptert melding: ", string(kryptertMelding))
+        _, err = conn.Write([]byte(string(kryptertMelding)))
 	if err != nil {
 		log.Fatal(err)
 	}
